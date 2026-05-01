36d681020c3b7ff4bac3a7940fbe847a488c8e3b
Mouhamed Lo <lom97131@gmail.com>
2026-05-01 16:31:15 +0000

    Setup initial Sentransport avec Header

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+function Header(){
+    return(
+        <header classeName="header">
+            <h1 classeName_="header-titre">Sentransport</h1>
+            <p classeName="header-soustitre">Votre guide du transport en commun a Dakar</p>
+        </header>
+    );
+}
+export default Header;
